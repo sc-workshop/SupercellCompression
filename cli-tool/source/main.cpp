3e8ffa8ef34c1e2933a280dc40491c9dad25a7fe
pavidloq <sokov302@gmail.com>
2024-06-09 21:40:26 +0300

    fixes

---
+++ cli-tool/source/main.cpp
@@ -0,0 +1,11 @@
+#include "compression/decompressor.h"
+#include "compression/compressor.h"
+
+#include "core/io/file_stream.h"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
